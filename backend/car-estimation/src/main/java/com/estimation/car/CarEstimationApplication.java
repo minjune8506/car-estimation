8faed31e60160d75818d3c5e8f711428357f74d3
Minjune <minjune8506@gmail.com>
2023-06-27 17:47:22 +0900

    chore: package 구조 변경

--- backend/car-estimation/src/main/java/com/estimation/car/CarEstimationApplication.java
+++ backend/car-estimation/src/main/java/com/estimation/car/CarEstimationApplication.java
@@ -1,4 +1,4 @@
-package com.estimation.car.carestimation;
+package com.estimation.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarEstimationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarEstimationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarEstimationApplication.class, args);
+    }
 
 }
