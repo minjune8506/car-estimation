fcea00739e3d6e99612befd9e0fbb49472ffaafb
Minjune <minjune8506@gmail.com>
2023-06-29 01:03:04 +0900

    [Backend] feat: controlleradvice 추가

---
+++ backend/car-estimation/src/main/java/com/estimation/car/exception/ExceptionAdvice.java
@@ -0,0 +1,15 @@
+package com.estimation.car.exception;
+
+import com.estimation.car.common.response.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse<Void>> handleNotFoundError(NotFoundException e) {
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                             .body(ErrorResponse.of(e.getErrorCode()));
+    }
+}
---
+++ backend/car-estimation/src/main/java/com/estimation/car/exception/NotFoundException.java
@@ -0,0 +1,14 @@
+package com.estimation.car.exception;
+
+import com.estimation.car.common.code.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class NotFoundException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public NotFoundException(ErrorCode code) {
+        super();
+        this.errorCode = code;
+    }
+}
