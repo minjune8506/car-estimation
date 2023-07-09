e9a97ed43d348f355ad44403a3dfcc573fad4148
Minjune <minjune8506@gmail.com>
2023-07-09 21:19:59 +0900

    [FrontEnd] chore: import 절대경로 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      src: "/src",
+      common: "/src/common",
+      components: "/src/components",
+      hooks: "/src/hooks",
+      pages: "/src/pages",
+      states: "/src/states",
+      types: "/src/types",
+    },
+  },
+});
